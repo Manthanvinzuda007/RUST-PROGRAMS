be3e9a097751e5f1b74d677c07b91a6dc1f4e319
Manthan Vinzuda <206708551+Manthanvinzuda007@users.noreply.github.com>
2026-01-10 11:58:00 +0530

    Update User Input.rs

--- User Input.rs
+++ User Input.rs
@@ -1,1 +1,18 @@
+use std::io; // 1. Input/Output Library 
+
+fn main() {
+    println!("What Is Your Pretty Name?"); // Print In Screen
+
+    // 2. Create Mutable Variable ( Why? Because Mutable Variable Is Changeble )
+    let mut name = String::new();
+
+    // 3. Put User Input 
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Input wanchva ma error aavi");
+
+    // 4. Output print
+    // .trim() Use Because The Extra Space Cancle If User press Enter 
+    println!("Namaste {}! Rust World ma swagat chhe.", name.trim());
+}
 
